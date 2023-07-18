9720ef4f3e9b8af0a8427a8b910bd1d22f0a5756
Edison <102976460+Ramiringuiss@users.noreply.github.com>
2023-07-18 16:36:21 -0500

    Update adult-comandos+18.js

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/Ramiringuiss/back/master/src/JSON/+18-${command}.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/Ramiringuiss/back/master/src/JSON/18-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]    
 conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
 //conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)    
